5696fd561b71115262de3517370d086f547d041b
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-12-19 11:29:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Week_6_Challenge_Labs._1
+{
+    internal class Program
+    {
+        static int SingleNumber(int[] nums)
+        {
+            int result = 0;
+
+            foreach (int num in nums)
+            {
+                result ^= num;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 2, 2, 1 };
+            int[] nums2 = { 4, 1, 2, 1, 2 };
+            int[] nums3 = { 1 };
+
+            Console.WriteLine(SingleNumber(nums1)); 
+            Console.WriteLine(SingleNumber(nums2)); 
+        }
+    }
+}
